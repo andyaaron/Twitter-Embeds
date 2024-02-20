36ea4e0c901c16e18ee1ba2cc2d38c7dfa758aa9
andyaaron <aaron.richard.andy@gmail.com>
2024-02-20 17:30:25 -0500

    update array

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ def hello():
 def get_twitter_screenshot():
     params_whitelist = [
         'url',
-        'filename',
-        # Add other accepted params as neededˆ
+        'filename'
     ]
 
     # Get params from request
@@ -129,4 +128,4 @@ def whitelist_and_sanitize(params, params_whitelist):
 
 if __name__ == '__main__':
     print('we out here')
-    app.run(host='127.0.0.1', port=80, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
